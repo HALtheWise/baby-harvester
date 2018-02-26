b7693176496831f777630f7b0d00c4426657e2e4
Eric Miller <eric@legoaces.org>
2018-02-26 17:02:46 -0500

    Added framework for developing pieces of client code

---
+++ client/src/gpio.rs
@@ -0,0 +1,15 @@
+
+pub fn set_bell(ringing: bool){
+    let _ignore = ringing;
+    return;
+}
+
+pub fn set_light(on: bool){
+    let _ignore = on;
+    return;
+}
+
+pub fn get_button() -> bool{
+    false
+}
+
