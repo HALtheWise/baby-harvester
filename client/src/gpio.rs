1b49ecd4360c972d556418b637cce4ab4d885e62
Eric Miller <eric@legoaces.org>
2018-03-05 02:04:38 -0500

    Added error handling for non-Raspi devices

--- client/src/gpio.rs
+++ client/src/gpio.rs
@@ -6,21 +6,34 @@ use std::time::Duration;
 
 use gpio::rppal::gpio::{Gpio, Mode, Level};
 use gpio::rppal::system::DeviceInfo;
+use gpio::rppal::system::Error;
 
 // The GPIO module uses BCM pin numbering. BCM 18 equates to physical pin 12.
 const LED_PIN:u8 = 18;
 
 pub fn setup(){
-    let device_info = DeviceInfo::new().unwrap();
-    println!("Model: {} (SoC: {})", device_info.model(), device_info.soc());
+    let device_info = DeviceInfo::new();
 
-    let mut gpio = Gpio::new().unwrap();
-    gpio.set_mode(LED_PIN, Mode::Output);
+    let is_raspi = match device_info {
+        Ok(device_info)=>{
+            println!("Model: {} (SoC: {})", device_info.model(), device_info.soc());
+            true
+            },
+        Err(error) => {
+            println!("Error encountered, perhaps this is a laptop?, {}", error);
+            false
+        }
+    };
 
-    // Blink an LED attached to the pin on and off
-    gpio.write(LED_PIN, Level::High);
-    thread::sleep(Duration::from_millis(500));
-    gpio.write(LED_PIN, Level::Low);
+    println!("This device {} a Raspi", if is_raspi { "is" }else{ "is not"})
+//
+//    let mut gpio = Gpio::new().unwrap();
+//    gpio.set_mode(LED_PIN, Mode::Output);
+//
+//    // Blink an LED attached to the pin on and off
+//    gpio.write(LED_PIN, Level::High);
+//    thread::sleep(Duration::from_millis(500));
+//    gpio.write(LED_PIN, Level::Low);
 }
 
 pub fn set_bell(ringing: bool){
