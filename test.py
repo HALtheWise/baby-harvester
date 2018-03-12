4facb50ee219ea5d4eed8802483765cfc7cb9880
Baby Harvester <babyharvester@olin.edu>
2018-03-12 20:39:45 +0000

    Working browser from mqtt

--- test.py
+++ test.py
@@ -8,8 +8,15 @@ import paho.mqtt.client as mqtt
 from urllib.parse import urlparse
 from subprocess import run
 
+
+chrome_options = Options()
+chrome_options.add_argument("--kiosk")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+_PRINTER_PORT = "usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0".encode()
+
 try:
-    printer = Adafruit_Thermal(port="/dev/ttyUSB0".encode(), baudrate=9600, timeout=5)
+    printer = Adafruit_Thermal(port=_PRINTER_PORT, baudrate=9600, timeout=5)
 except SerialException:
     print("Problem accessing printer")
 
@@ -26,14 +33,19 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    body = str(msg.payload)
+    body = msg.payload.decode('utf-8')
     print(msg.topic+" "+body)
     if msg.topic == (dev_name + "/print/text"):
         print("printing")
-        # printer.println(body) #seems to just print to stdout
-        with open('/dev/ttyUSB0', 'w') as printbuf:
-            printbuf.write(body+"\\n\\n") 
+        #printer.println(body+"\\n")
+        #printer.feed(3)
+        with open(str(_PRINTER_PORT), "w") as printbuf:
+            printbuf.write(body+"\\n")
+            printbuf.write("\\n\\n")
         print("printed")
+    if msg.topic == (dev_name + "/display/url"):
+        print(body)
+        driver.get(body)
     else:
         pass
 
@@ -48,9 +60,3 @@ client.connect(conn_url.hostname, conn_url.port, 60)
 print('connected')
 client.loop_forever()
 
-#chrome_options = Options()
-#chrome_options.add_argument("--kiosk")
-#
-#driver = webdriver.Chrome(chrome_options=chrome_options)
-#
-#driver.get('https://google.com')
