a79437dd482190bd3324bb21803497da8888d7b5
Eric Miller <eric@legoaces.org>
2018-03-17 01:09:52 -0400

    Added error handling for running not on a Raspi

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ import base64
 try:
     from gpiozero import Button
 except Exception:
-    print("Unable to start GPIO")
+    print("Unable to start GPIO, you may not be running on a Raspberry Pi.")
+    Button = None
 
 # Monkey patch Adadruit_Thermal
 old_write = Adafruit_Thermal.write
@@ -22,6 +23,7 @@ old_write = Adafruit_Thermal.write
 def newWrite(self, *data):
     data = [s.encode() for s in data]
     old_write(self, *data)
+
 
 def newWriteBytes(self, *args):
     if self.writeToStdout:
@@ -32,6 +34,7 @@ def newWriteBytes(self, *args):
         self.timeoutSet(len(args) * self.byteTime)
         for arg in args:
             super(Adafruit_Thermal, self).write([arg])
+
 
 Adafruit_Thermal.write = newWrite
 Adafruit_Thermal.writeBytes = newWriteBytes
@@ -57,24 +60,35 @@ try:
     printer = Adafruit_Thermal(str("/dev/serial/by-id/{}".format(_PRINTER_PORT)), 9600, timeout=5)
 except SerialException as e:
     print("Problem accessing printer", e)
+    printer = lambda: None
+    printer.println = lambda s: print("Printer would have printed '{}'".format(s))
+    printer.feed = lambda n: None
 
 mqtt_url = str(os.environ["MQTT_URL"])
 dev_name = str(os.environ["DEVICE_NAME"])
 app_secret = str(os.environ["APP_SECRET"])
+api_root = str(os.environ["API_ROOT"])
 
 conn_url = urlparse(mqtt_url)
+
 
 def change_token():
     """Should hit the gateway, and the gateway will return a print of the new auth token"""
     print("changing token")
-    requests.get('https://baby-harvester-gateway.herokuapp.com/changetoken', auth=(dev_name, app_secret))
+    requests.get(api_root + '/changetoken', auth=(dev_name, app_secret))
+
 
 def shutdown():
-        run(['sudo', 'poweroff'])
+    run(['sudo', 'poweroff'])
 
-button = Button(2, hold_time=3)
-button.when_released = change_token
-button.when_held = shutdown
+
+if Button:
+    button = Button(2, hold_time=3)
+    button.when_released = change_token
+    button.when_held = shutdown
+else:
+    button = None
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -104,7 +118,6 @@ def on_message(client, userdata, msg):
         pass
 
 
-
 client = mqtt.Client(client_id=dev_name)
 client.on_connect = on_connect
 client.on_message = on_message
@@ -114,4 +127,9 @@ client.username_pw_set(conn_url.username, password=conn_url.password)
 print('connecting...')
 client.connect(conn_url.hostname, conn_url.port, 60)
 print('connected')
+
+if len(sys.argv) > 1 and sys.argv[1] == 'auth':
+    from threading import Timer
+    Timer(5, change_token).start()
+
 client.loop_forever()
